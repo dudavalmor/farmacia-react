f74b0481b571b35efd90644782bdfb23b22a13b7
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-01-28 09:10:37 -0300

    correção da versão do tailwind: 4.0.0 para 3.4.17

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="flex flex-col items-center">
+                    <h2 className="text-3x1 font-bold">Bem vindo a farma(gen)</h2>
+                    <p>Sua farmácia de manipulados on-line</p>
+                    </div>
+
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://img.freepik.com/fotos-gratis/capsulas-medicas-de-close-up-com-ervas-em-cima-da-mesa_23-2148431497.jpg?t=st=1738033173~exp=1738036773~hmac=a682b585a917ce85b21c70ab3ca3f79040a46a86b06665997c164dc471f37253&w=740"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
